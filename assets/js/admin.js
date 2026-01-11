832d2d46c39c55adf7ec884dafb6c68a96504d73
N1kitaDev <hellopollymolly712@gmail.com>
2026-01-11 21:00:48 +0300

    Fix: Recreate missing plugin files (widget, CSS, JS, translations)

---
+++ assets/js/admin.js
@@ -0,0 +1,11 @@
+// Admin JavaScript for Strix Google Reviews
+jQuery(document).ready(function($) {
+    'use strict';
+
+    console.log('Strix Google Reviews Admin JS loaded');
+
+    // Add any admin-specific JavaScript here
+    $('.strix-google-reviews-admin').on('click', '.notice-dismiss', function() {
+        // Handle notice dismissal
+    });
+});
